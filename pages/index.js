6210cb6598434d6acb99343d4aa7c4e2341d0749
kandarp dangi <kandarpdangi@gmail.com>
2021-08-25 11:32:31 +0530

    init styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Head from 'next/head'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+	font-size: 50px;
+	color: ${({ theme }) => theme.colors.primary.dark};
+`
 
 export default function Home() {
 	return (
 		<div>
-			<></>
+			<Head>
+				<title>Sign in </title>
+			</Head>
+			<Title>hii</Title>
 		</div>
 	)
 }
